4aca54e08e83c3dcef59a6ade0882f1b78f72f7c
tison2810 <son.nguyenthai@hcmut.edu.vn>
2023-12-05 23:21:52 +0700

    component cho máy in

--- spss/src/views/ChoosePrinter.jsx
+++ spss/src/views/ChoosePrinter.jsx
@@ -8,9 +8,58 @@ import logo_hcmut from '../images/logo_hcmut.png';
 
 export default function ChoosePrinter() {
   const [printerInfo, setPrinterInfo] = React.useState(false);
-  const togglePrinterInfo = () => {
-      setPrinterInfo(!printerInfo);
+  const [selectedPrinter, setSelectedPrinter] = React.useState(null);
+  const togglePrinterInfo = (printer) => {
+    setPrinterInfo(!printerInfo);
+    setSelectedPrinter(printer);
   }
+  const printerData = [
+    { id: "1", state: "busy", label: "Máy in màu Epson L121" },
+    { id: "2", state: "unavailable", label: "Máy in màu Epson L121" },
+    { id: "3", state: "free", label: "Máy in màu Epson L121" },
+    { id: "4", state: "free", label: "Máy in màu Epson L121" },
+    { id: "5", state: "free", label: "Máy in màu Epson L121" },
+    { id: "6", state: "unavailable", label: "Máy in màu Epson L121" },
+    { id: "7", state: "busy", label: "Máy in màu Epson L121" },
+    { id: "8", state: "unavailable", label: "Máy in màu Epson L121" },
+    { id: "9", state: "free", label: "Máy in màu Epson L121" },
+    { id: "10", state: "free", label: "Máy in màu Epson L121" },
+    { id: "11", state: "free", label: "Máy in màu Epson L121" },
+    { id: "12", state: "unavailable", label: "Máy in màu Epson L121" },
+    { id: "13", state: "busy", label: "Máy in màu Epson L121" },
+    { id: "14", state: "unavailable", label: "Máy in màu Epson L121" },
+    { id: "15", state: "free", label: "Máy in màu Epson L121" },
+    { id: "16", state: "free", label: "Máy in màu Epson L121" },
+    { id: "17", state: "free", label: "Máy in màu Epson L121" },
+    { id: "18", state: "unavailable", label: "Máy in màu Epson L121" },
+    { id: "19", state: "busy", label: "Máy in màu Epson L121" },
+    { id: "20", state: "unavailable", label: "Máy in màu Epson L121" },
+  ];
+
+  const getStateText = (state) => {
+    if (state === "busy") {
+      return "Bận";
+    } else if (state === "unavailable") {
+      return "Không có sẵn";
+    } else if (state === "free") {
+      return "Trống";
+    }
+    return "";
+  };
+
+  const getStateTextClass = (state) => {
+    switch (state) {
+      case "busy":
+        return style["busy-text"];
+      case "unavailable":
+        return style["unavailable-text"];
+      case "free":
+        return style["free-text"];
+      default:
+        return ""; // Mặc định nếu trạng thái không khớp
+    }
+  };
+
   return (
     <React.Fragment>
       <nav>
@@ -39,68 +88,28 @@ export default function ChoosePrinter() {
           <Link to='/specify-properties'><button>Quay lại</button></Link>
         </div>
         <div className={style.content}>
-           <div className={style.item}>
-            <div className={style.image} id="free">
-              <img src={printer_icon} alt="printer-img" onClick={togglePrinterInfo} />
-            </div>
-            <div className={style.label}>Máy in màu Epson L121</div>
-          </div>
-          <div className={style.item} id="busy">
-            <div className={style.image}>
-              <img src={printer_icon} alt="printer-img" onClick={togglePrinterInfo}/>
-            </div>
-            <div className={style.label}>Máy in màu Epson L121</div>
-          </div>
-          <div className={style.item} id="unvailable">
-            <div className={style.image}>
-              <img src={printer_icon} alt="printer-img" />
-            </div>
-            <div className={style.label}>Máy in màu Epson L121</div>
-          </div>
-          {/* <div className={style.item}>
-          <div className={style.image}>
-            <img src={printer_icon} alt="printer-img" />
-          </div>
-          <div className={style.label}>Máy in màu Epson L121</div>
-        </div>
-        <div className={style.item}>
-          <div className={style.image}>
-            <img src={printer_icon} alt="printer-img" />
-          </div>
-          <div className={style.label}>Máy in màu Epson L121</div>
-        </div>
-        <div className={style.item}>
-          <div className={style.image}>
-            <img src={printer_icon} alt="printer-img" />
-          </div>
-          <div className={style.label}>Máy in màu Epson L121</div>
-        </div>
-        <div className={style.item}>
-          <div className={style.image}>
-            <img src={printer_icon} alt="printer-img" />
-          </div>
-          <div className={style.label}>Máy in màu Epson L121</div>
-        </div>
-        <div className={style.item}>
-          <div className={style.image}>
-            <img src={printer_icon} alt="printer-img" />
-          </div> 
-          <div className={style.label}>Máy in màu Epson L121</div>
-        </div> */}
-
-            {printerInfo && 
-            (<div className={style.printer_info_free} style={{ display: 'flex' }}>
-            <div className={style.temp}>
-              <div className={style.name_and_img}>
-                <div id="name">Máy in màu Epson L121</div>
-                <div id="img">
-                  <img src={printer_icon} alt="printer-img" />
-                </div>
+          {printerData.map((printer) => (
+            <div className={style.item} id={printer.id}>
+              <div className={style.image}>
+                <img src={printer_icon} alt="printer-img" onClick={() => togglePrinterInfo(printer)} />
               </div>
+              <div className={style.label}>{printer.label}</div>
+            </div>
+          ))}
+
+          {printerInfo &&
+            (<div className={style.printer_info_show} style={{ display: 'flex' }}>
+              <div className={style.temp}>
+                <div className={style.name_and_img}>
+                  <div id={style.name}>Máy in màu Epson L121</div>
+                  <div id="img">
+                    <img src={printer_icon} alt="printer-img" />
+                  </div>
+                </div>
 
                 <div className={style.info}>
                   <div>
-                    <p>Tình trạng: <span>Trống</span></p>
+                    <p>Tình trạng: <span className={style[getStateTextClass(selectedPrinter.state)]}>{getStateText(selectedPrinter.state)}</span></p>
                   </div>
                   <div><p>Hàng đợi: 0</p></div>
                   <div><p>Loại: In màu</p></div>
@@ -109,68 +118,13 @@ export default function ChoosePrinter() {
                   <div><p>Vị trí: Thư viện A2</p></div>
                   <div>
                     <button onClick={togglePrinterInfo}>Quay lại</button>
-                    <Link to = '/pay-order'><button>Xác nhận</button></Link>
+                    <Link to='/pay-order'><button>Xác nhận</button></Link>
                   </div>
                 </div>
+              </div>
             </div>
-          </div>
             )}
         </div>
-        {/* {printerInfo && (<div className={style.printer_info_busy} style={{ display: 'flex' }} >
-          <div className={style.temp}>
-            <div className={style.name_and_img}>
-              <div id="name">Máy in màu Epson L121</div>
-              <div id="img">
-                <img src={printer_icon} alt="printer-img" />
-              </div>
-            </div>
-
-            <div className={style.info}>
-              <div>
-                <p>Tình trạng: <span>Đang in</span></p>
-              </div>
-              <div>
-                <p>Hàng đợi: <span>02</span></p>
-              </div>
-              <div><p>Loại: In màu</p></div>
-              <div><p>Xuất xứ: Việt Nam</p></div>
-              <div><p>Mã máy: 0x02</p></div>
-              <div><p>Vị trí: Thư viện A2</p></div>
-              <div>
-                <a href="#" id="close_busy"><button>Quay lại</button></a>
-                <a href="PayOrder.html"><button>Xác nhận</button></a>
-              </div>
-            </div>
-          </div>
-        </div>
-        )}
-        <div className={style.printer_info_unavailable}>
-          <div className={style.temp}>
-            <div className={style.name_and_img}>
-              <div id="name">Máy in màu Epson L121</div>
-              <div id="img">
-                <img src={printer_icon} alt="printer-img" />
-              </div>
-            </div> */}
-
-            {/* <div className={style.info}>
-              <div>
-                <p>Tình trạng: <span>Bảo trì</span></p>
-              </div>
-              <div>
-                <p>Hàng đợi: 0</p>
-              </div>
-              <div><p>Loại: In màu</p></div>
-              <div><p>Xuất xứ: Việt Nam</p></div>
-              <div><p>Mã máy: 0x03</p></div>
-              <div><p>Vị trí: Thư viện A2</p></div>
-              <div><p>Vui lòng chọn máy khác</p></div>
-              <div>
-                <a href="#" id="close_unavailable"><button>Quay lại</button></a>
-              </div>
-            </div> */}
-          {/* </div> */}
-        {/* </div> */}
       </div>
     </React.Fragment>
   );
