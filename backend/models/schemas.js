73f3f43651477c83c2f3d684c4b885726d9e1bff
tison2810 <son.nguyenthai@hcmut.edu.vn>
2023-12-07 09:43:15 +0700

    add printer to backend

--- backend/models/schemas.js
+++ backend/models/schemas.js
@@ -8,4 +8,13 @@ const userSchema = new Schema({
 
 const Users = mongoose.model('users', userSchema)
 
+const printerSchema = new Schema({
+    id: {type: String},
+    status: {type: String},
+    lable: {type: String},
+    place: {type: String}
+})
+const Printers = mongoose.model('printers', printerSchema,'printers')
+
+module.exports = Printers
 module.exports = Users
